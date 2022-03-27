07e6e2589831a9d1e4fdfe45d4ce54fcbdbd9ba0
Siarhey Averburh <Averbyrg@gmail.com>
2022-03-27 22:52:12 +0300

    Homework-19 from Siarhey Averburh

--- app/src/main/java/com/example/lessson17/MainActivity.kt
+++ app/src/main/java/com/example/lessson17/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.lessson17
 
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -11,6 +14,7 @@ import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private var counter = 0
@@ -18,34 +22,34 @@ class MainActivity : AppCompatActivity() {
 
     @DrawableRes
     private var currentImageRes = R.drawable.cat
-
     private lateinit var infoTextView: TextView
     private lateinit var rootView: View
     private lateinit var imageView: ImageView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        appContext = applicationContext
         rootView = findViewById(R.id.root)
         infoTextView = findViewById(R.id.tv_info)
         imageView = findViewById(R.id.imageView)
 
 
         findViewById<View>(R.id.btn_counter_minus).setOnClickListener {
-            updateCounter(counter - 1)
+            updateCounter(counter - resources.getInteger(R.integer.counter_step))
         }
         findViewById<View>(R.id.btn_counter_plus).setOnClickListener {
-            updateCounter(counter + 1)
+            updateCounter(counter + resources.getInteger(R.integer.counter_step))
 
         }
         findViewById<View>(R.id.btn_counter_rnd).setOnClickListener {
-            updateCounter((-100..100).random())
+            val rangeForRND =
+                resources.getInteger(R.integer.min_value_for_rnd)..resources.getInteger(R.integer.max_value_for_rnd)
+            updateCounter((rangeForRND).random())
 
         }
         findViewById<View>(R.id.btn_counter_0).setOnClickListener {
-            updateCounter(0)
+            updateCounter(resources.getInteger(R.integer.start_value_for_counter))
         }
 
         findViewById<View>(R.id.btn_color_r).setOnClickListener {
@@ -62,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         imageView.setOnClickListener {
-            updateRotation(rotation + 90)
+            updateRotation(rotation + resources.getInteger(R.integer.swivel_angle_by_click).toFloat())
         }
     }
 
@@ -73,13 +77,15 @@ class MainActivity : AppCompatActivity() {
 
     fun setBg(view: View) {
         val colorText = when ((view as Button).text) {
-            "1" -> "#cccccc"
-            "2" -> "#dddddd"
-            "3" -> "#eeeeee"
-            else -> "#ffffff"
+            getString(R.string.number_1) -> getColor(R.color.background_gray)
+            getString(R.string.number_2) -> getColor(R.color.background_light_gray)
+            getString(R.string.number_3) -> getColor(R.color.background_ivory)
+            else -> getColor(R.color.background_white)
         }
-        val color = Color.parseColor(colorText)
-        rootView.setBackgroundColor(color)
+        rootView.setBackgroundColor(colorText)
+        Log.d(
+            TAG, "click on button ${view.text} set color $colorText"
+        )
     }
 
     fun setImage(view: View) {
@@ -88,9 +94,12 @@ class MainActivity : AppCompatActivity() {
         if (imageRes == null) {
             imageRes = (IMAGES_MAP.values - currentImageRes).random()
         }
-        updateRotation(0f)
+        updateRotation(resources.getInteger(R.integer.swivel_angle_start).toFloat())
         currentImageRes = imageRes
         imageView.setImageResource(imageRes)
+        Log.d(
+            TAG, "click on button ${view.text} set image $imageRes"
+        )
     }
 
     private fun updateRotation(angle: Float) {
@@ -99,26 +108,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private val IMAGES_MAP = mapOf(
-            "cat" to R.drawable.cat,
-            "dog" to R.drawable.dog,
-            "parrot" to R.drawable.parrot,
-        )
+        private lateinit  var appContext: Context
+        private val IMAGES_MAP by lazy {
+            mapOf(
+                appContext.getString(R.string.cat) to R.drawable.cat,
+                appContext.getString(R.string.dog) to R.drawable.dog,
+                appContext.getString(R.string.parrot) to R.drawable.parrot,
+            )
+        }
     }
 
     fun info(view: View) {
         when ((view as Button).text) {
-            "device" -> {
+            getString(R.string.device) -> {
                 infoTextView.text = android.os.Build.MANUFACTURER + " " + android.os.Build.MODEL
 
             }
-            "time" -> {
-                infoTextView.text = SimpleDateFormat("HH:mm", Locale.US).format(Date())
+            getString(R.string.time) -> {
+                infoTextView.text =
+                    SimpleDateFormat(getString(R.string.date_format), Locale.US).format(Date())
             }
-            "toast" -> {
-                Toast.makeText(this, "hello", Toast.LENGTH_LONG).show()
+            getString(R.string.toast) -> {
+                Toast.makeText(this, getString(R.string.hello_message), Toast.LENGTH_LONG).show()
             }
-            else -> error("unknown command")
+            else -> error(getString(R.string.unknown_command))
         }
+        Log.d(
+            TAG, "click on button ${view.text}"
+        )
     }
 }
