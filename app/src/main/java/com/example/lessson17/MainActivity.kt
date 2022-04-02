89597273dd7d196e966c1d5de555c87c2f6124c0
GansA11es <sfsfvl33@mail.ru>
2022-04-02 22:30:57 +0300

    Working on the bugs, by Ilya Sibogatov

--- app/src/main/java/com/example/lessson17/MainActivity.kt
+++ app/src/main/java/com/example/lessson17/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.lessson17
 
-import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -14,62 +12,54 @@ import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
-    private var counter = 0
-    private var rotation = 0f
+    private var counter = ENTRY_COUNTER
+    private var rotation = IMAGE_ANGLE_START
 
     @DrawableRes
     private var currentImageRes = R.drawable.cat
-
     private lateinit var infoTextView: TextView
     private lateinit var rootView: View
     private lateinit var imageView: ImageView
+    private lateinit var imageZooMap: Map<String, Int>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val MIN_VALUE = resources.getInteger(R.integer.minValues)
-        val MAX_VALUE = resources.getInteger(R.integer.maxValues)
-
-        draw = applicationContext
+        val minValue = resources.getInteger(R.integer.min_value)
+        val maxValue = resources.getInteger(R.integer.max_value)
 
         rootView = findViewById(R.id.root)
         infoTextView = findViewById(R.id.tv_info)
         imageView = findViewById(R.id.imageView)
 
+        imageZooMap =
+            mapOf(
+                getString(R.string.button_cat) to R.drawable.cat,
+                getString(R.string.button_dog) to R.drawable.dog,
+                getString(R.string.button_parrot) to R.drawable.parrot
+            )
 
         findViewById<View>(R.id.btn_counter_minus).setOnClickListener {
-            updateCounter(counter - 1)
+            updateCounter(counter - COUNTER_STEP)
         }
         findViewById<View>(R.id.btn_counter_plus).setOnClickListener {
-            updateCounter(counter + 1)
+            updateCounter(counter + COUNTER_STEP)
 
         }
         findViewById<View>(R.id.btn_counter_rnd).setOnClickListener {
-            updateCounter((MIN_VALUE..MAX_VALUE).random())
+            updateCounter((minValue..maxValue).random())
 
         }
         findViewById<View>(R.id.btn_counter_0).setOnClickListener {
-            updateCounter(0)
-        }
-
-        findViewById<View>(R.id.btn_color_r).setOnClickListener {
-            infoTextView.setTextColor(Color.RED)
-        }
-        findViewById<View>(R.id.btn_color_g).setOnClickListener {
-            infoTextView.setTextColor(Color.GREEN)
-        }
-        findViewById<View>(R.id.btn_color_b).setOnClickListener {
-            infoTextView.setTextColor(Color.BLUE)
-        }
-        findViewById<View>(R.id.btn_color_m).setOnClickListener {
-            infoTextView.setTextColor(Color.MAGENTA)
+            updateCounter(ENTRY_COUNTER)
         }
 
         imageView.setOnClickListener {
-            updateRotation(rotation + 90)
+            updateRotation(rotation + ROTATION_BY_CLICK)
         }
     }
 
@@ -79,47 +69,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setBg(view: View) {
-        val colorText = when ((view as Button).text) {
-            "1" -> getColor(R.color.white_204)
-            "2" -> getColor(R.color.white_221)
-            "3" -> getColor(R.color.white_238)
+        val colorBg = when ((view as Button).text) {
+            getString(R.string.button_1) -> getColor(R.color.white_204)
+            getString(R.string.button_2) -> getColor(R.color.white_221)
+            getString(R.string.button_3) -> getColor(R.color.white_238)
             else -> getColor(R.color.white)
         }
-        rootView.setBackgroundColor(colorText)
+        rootView.setBackgroundColor(colorBg)
 
         Log.d(TAG, "Button click ${view.text}")
     }
 
-    fun setImage(view: View) {
-        val text = (view as Button).text
-        var imageRes = IMAGES_MAP[text]
-        if (imageRes == null) {
-            imageRes = (IMAGES_MAP.values - currentImageRes).random()
+    fun setColorText(view: View) {
+        val colorText = when ((view as Button).text) {
+            getString(R.string.button_r) -> getColor(R.color.red)
+            getString(R.string.button_g) -> getColor(R.color.green)
+            getString(R.string.button_b) -> getColor(R.color.blue)
+            getString(R.string.button_m) -> getColor(R.color.magenta)
+            else -> getColor(R.color.black)
         }
-        updateRotation(0f)
+        infoTextView.setTextColor(colorText)
+        Log.d(TAG, "Button click ${view.text}")
+    }
+
+    fun setImage(view: View) {
+        val text = ((view as Button).text)
+        var imageRes = imageZooMap[text]
+        if (imageRes == null) {
+            imageRes = (imageZooMap.values - currentImageRes).random()
+        }
+        updateRotation(IMAGE_ANGLE_START)
         currentImageRes = imageRes
         imageView.setImageResource(imageRes)
-
         Log.d(TAG, "Button click ${view.text}")
     }
 
     private fun updateRotation(angle: Float) {
         rotation = angle
         imageView.rotation = rotation
-    }
-
-    companion object {
-
-        private const val TAG = "PrivateActivity"
-
-        private lateinit var draw: Context
-        private val IMAGES_MAP by lazy {
-            mapOf(
-                draw.getString(R.string.button_cat) to R.drawable.cat,
-                draw.getString(R.string.button_dog) to R.drawable.dog,
-                draw.getString(R.string.button_parrot) to R.drawable.parrot
-            )
-        }
     }
 
     fun info(view: View) {
@@ -136,5 +123,14 @@ class MainActivity : AppCompatActivity() {
             else -> error(resources.getString(R.string.error))
         }
         Log.d(TAG, "Button click ${view.text}")
+    }
+
+    companion object {
+
+        private const val TAG = "PrivateActivity"
+        private const val COUNTER_STEP = 1
+        private const val ENTRY_COUNTER = 0
+        private const val ROTATION_BY_CLICK = 90f
+        private const val IMAGE_ANGLE_START = 0f
     }
 }
